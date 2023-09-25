6ba2711abbabc4bcd40fb2050675bd4cd0124938
SaneMyburg <sane.myburg@gmail.com>
2023-09-25 13:58:42 +0200

    Add motorbike model validations

--- app/models/motorbike.rb
+++ app/models/motorbike.rb
@@ -1,4 +1,8 @@
 class Motorbike < ApplicationRecord
   belongs_to :user
   has_many :reservations, dependent: :destroy
+
+  validates :price, numericality: { greater_than: 0 }
+  validates :name, length: { maximum: 255 }
+  validates :description, length: { maximum: 1000 } 
 end
