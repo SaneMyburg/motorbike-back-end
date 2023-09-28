59ef6415fef009acbe25a93c766051817c56d26e
SaneMyburg <sane.myburg@gmail.com>
2023-09-28 14:34:00 +0200

    Add more attributes in motorbike model

--- app/models/motorbike.rb
+++ app/models/motorbike.rb
@@ -1,8 +1,10 @@
 class Motorbike < ApplicationRecord
-  belongs_to :user
   has_many :reservations, dependent: :destroy
 
   validates :price, numericality: { greater_than: 0 }
   validates :name, length: { maximum: 255 }
   validates :description, length: { maximum: 1000 }
+  validates :finance_fee, presence: true
+  validates :total_amount_payable, presence: true
+  validates :duration, presence: true
 end
