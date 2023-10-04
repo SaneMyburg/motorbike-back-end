3d65f13991a30c026e7bf6efc5f89eeff9344a1f
SaneMyburg <sane.myburg@gmail.com>
2023-10-04 11:59:13 +0200

    Add user requests spec

---
+++ spec/requests/api/users_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/users', type: :request do
+  path '/api/v1/users' do
+    post('new_session user') do
+      consumes 'application/json'
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string }
+
+        },
+        required: ['name']
+      }
+
+      response '201', 'user created' do
+        let(:user) { { name: 'Ali' } }
+        run_test!
+      end
+
+      response '422', 'invalid request' do
+        let(:user) { { name: 'Ali' } }
+        run_test!
+      end
+    end
+  end
+end
