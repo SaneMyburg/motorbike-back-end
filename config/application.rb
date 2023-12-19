d7fd59bee32b014e4b17cf9f71d84b669353b144
Sane Myburg <sane.myburg@gmail.com>
2023-12-19 13:33:37 +0200

    Add front end domain

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,13 @@ module MotorbikeBackEnd
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+     # Enable Rack CORS for handling Cross-Origin Resource Sharing
+     config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'https://motorbike-front-end.vercel.app/' # Replace with your actual frontend domain
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
+      end
+    end
   end
 end
